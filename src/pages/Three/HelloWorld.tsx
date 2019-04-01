878efb73c01fdc1c1938b8b7e3ec8493787a8c31
wangqiang <1432613064@qq.com>
2019-04-01 15:54:28 +0800

    优化页面滚动交互

--- src/pages/Three/HelloWorld.tsx
+++ src/pages/Three/HelloWorld.tsx
@@ -11,21 +11,26 @@ import BaseComponent from '@components/Base';
 
 export default class HelloWorld extends BaseComponent {
 
-  container: any = null;
+  container: HTMLDivElement = null;
 
   componentDidMount() {
-    this.init(this.container);
+    setTimeout(() => {
+      this.init();
+    }, 100);
   }
 
-  init = (current) => {
+  init = () => {
+    const { clientWidth, clientHeight } = this.container;
     const scene = new Scene();
-    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    current.appendChild(renderer.domElement);
+    const camera = new PerspectiveCamera(75, clientWidth / (clientHeight - 5), 0.1, 1000);
+    const renderer = new WebGLRenderer({
+      antialias: true
+    });
+    renderer.setSize(clientWidth, (clientHeight - 5));
+    this.container.appendChild(renderer.domElement);
     var geometry = new BoxGeometry(1, 1, 1);
     var material = new MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new Mesh(geometry, material); 
+    var cube = new Mesh(geometry, material);
     camera.position.z = 5;
     scene.add(cube);
     function render() {
@@ -39,7 +44,7 @@ export default class HelloWorld extends BaseComponent {
 
   render() {
     return (
-      <div ref={ref => this.container = ref} />
+      <div className="container" ref={ref => this.container = ref} />
     )
   }
 }
